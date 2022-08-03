a51738ece024067d2032c7a478c1babea7f3d3eb
ThandraDivya <divyathandra99@gmail.com>
2022-08-03 11:47:25 +0530

    removed the syso
    removed the syso

--- JavaGit/src/javagitone/Intialization.java
+++ JavaGit/src/javagitone/Intialization.java
@@ -1,10 +1,16 @@
 package javagitone;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Intialization {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
 	}
 
 }
